4bf0b5b1bd1eed2222f0430d72a77cae74bbf927
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-01 12:23:04 +0700

    setting navbar project olCourseV1 App

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -9,7 +9,7 @@ const NavnbarComponent = () => {
       {/* <Navbar expand="lg" className="bg-body-tertiary"> */}
       <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Course</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
