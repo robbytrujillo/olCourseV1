7cfa95e2a3c06bcf6154571546147488f4a2afd9
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-01 10:25:39 +0700

    add folder and file project olCourseV1 App

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,5 +1,7 @@
 import {Navbar, Container, Nav} from 'react-bootstrap';
 
+import {navLinks} from '../data/index';
+
 const NavnbarComponent = () => {
   return (
     <div>
@@ -10,8 +12,14 @@ const NavnbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            {navLinks.map((link) => {
+              return( <div>
+                <Nav.Link href="#home">Home</Nav.Link>
+              </div>
+              );
+            })}
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
