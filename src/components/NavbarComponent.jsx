c98aca4aa33892cb26d154101a134790ca20877f
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-05 06:07:39 +0700

    setting navbar project olCourseV1 App | homepage

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -12,7 +12,7 @@ const NavnbarComponent = () => {
         <Navbar.Brand href="#home" className='fs-3 fw-bold'>Course</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
+          <Nav className="mx-auto text-center">
             {navLinks.map((link) => {
               return( 
               <div className='nav-link' key={link.id}>
@@ -25,7 +25,7 @@ const NavnbarComponent = () => {
             })}
           </Nav>
 
-          <div>
+          <div className='text-center'>
             <button className='btn btn-outline-danger rounded-1'>Join With Us</button>
           </div>
         </Navbar.Collapse>
