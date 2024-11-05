0fe4435b613027d4d6e3d39575f6f16e7c521cfd
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-05 14:46:03 +0700

    setting navbar project olCourseV1 App | homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,13 +7,13 @@ const HomePage = () => {
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
           <Row className='header-box d-flex align-items-center'>
-            <Col>
+            <Col lg='6'>
               <h1 className='mb-4'>Temukan <br/> <span>Bakat Kreatifmu</span> <br /> Bersama Kami</h1>
               <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, distinctio expedita numquam molestias fuga veritatis!</p>
-              <button className='btn btn-danger btn-lg rounded-1 me-2'>Lihat Kelas</button>
-              <button className='btn btn-outline-danger btn-lg rounded-1'>Lihat Promo</button>
+              <button className='btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2'>Lihat Kelas</button>
+              <button className='btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2'>Lihat Promo</button>
             </Col>
-            <Col>
+            <Col lg='6' className='pt-lg-0 pt-5'>
             <img src={HeroImage} alt="hero-img" />
             </Col>
           </Row>
