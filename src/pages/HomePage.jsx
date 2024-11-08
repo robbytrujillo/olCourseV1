3d9c7cbb63dcc4e46fdafbc28bbb8e0c3fb3592d
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-08 10:55:20 +0700

    setting navbar project olCourseV1 App | homepage kelasTerbaru

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -34,7 +34,7 @@ const HomePage = () => {
               return ( 
               <Col key={kelas.id}>
                 <img src={kelas.image} alt="unplash.com" className='w-100 mb-5 rounded-top' />
-                  <div className='star mb-2'>
+                  <div className='star mb-2 px-3'>
                     <i className={kelas.star1}></i>
                     <i className={kelas.star2}></i>
                     <i className={kelas.star3}></i>
@@ -42,9 +42,9 @@ const HomePage = () => {
                     <i className={kelas.star5}></i>
                   </div>
                   <h5 className='mb-5 px-3'>{kelas.title}</h5>
-                  <div>
-                    <p>{kelas.price}</p>
-                    <button>{kelas.buy}</button>
+                  <div className='ket d-flex justify-content-between align-items-center px-3 pb-3'>
+                    <p className='m-0 text-primary fw-bold'>{kelas.price}</p>
+                    <button className='btn btn-danger rounded-1'>{kelas.buy}</button>
                   </div>
               </Col>
               );
