f812163317e6c3d29268a0b221397eca55b57c05
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-13 13:03:11 +0700

    setting navbar project olCourseV1 App | footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,9 +1,33 @@
-
+import {Container, Row, Col} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const FooterComponent = () => {
   return (
     <div>
-      FooterComponent
+      <Container>
+        <Row>
+          <Col>
+            <h3 className='fw-bold'>ihbsCounter.</h3>
+            <p className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum aliquid voluptatibus cumque ut maxime cupiditate odit ullam asperiores omnis!</p>
+            <div className='no'>
+              <Link>
+                <i className='fa-brands fa-whatsapp'></i>
+                <p>+62 123-4567-8910</p>
+              </Link>
+            </div>
+            <div className='mail'>
+              <Link>
+                <i className='fa-regular fa-envelope'></i>
+                <p>ihbscounter@gmail.com</p>
+              </Link>
+            </div>
+          </Col>
+          <Col>
+          
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   )
 }
