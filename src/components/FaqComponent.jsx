3ffe705339067ec8a920b58a83404afbc3322ca6
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-13 12:48:14 +0700

    setting navbar project olCourseV1 App | homepage FAQ

--- src/components/FaqComponent.jsx
+++ src/components/FaqComponent.jsx
@@ -19,16 +19,10 @@ const FaqComponent = () => {
               return(
                 <Col key={data.id}>
                   <Accordion className='shadow-sm'>
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                  <Accordion.Item eventKey={data.eventKey}>
+                    <Accordion.Header>{data.title}</Accordion.Header>
                       <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        {data.desc}
                       </Accordion.Body>
                     </Accordion.Item>
                   </Accordion>
