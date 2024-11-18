7fdb650772e6e67f3c44ee0bea7cae285f56aa36
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-19 05:47:21 +0700

    setting navbar project olCourseV1 App | kelas page

--- src/pages/KelasPage.jsx
+++ src/pages/KelasPage.jsx
@@ -3,12 +3,12 @@ import {semuaKelas} from '../data/index';
 
 const KelasPage = () => {
   return (
-    <div className='kelas'>
+    <div className='kelas min-vh-100'>
       <Container>
         <Row>
           <Col>
-            <h1>Semua Kelas</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <h1 className='fw-bold text-center'>Semua Kelas</h1>
+            <p className='text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
           </Col>
         </Row>
       </Container>
