7645afc775302f25ebe7c042d2c895117c12aa60
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-19 05:19:44 +0700

    setting navbar project olCourseV1 App | kelas page

--- src/pages/KelasPage.jsx
+++ src/pages/KelasPage.jsx
@@ -1,7 +1,17 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import {semuaKelas} from '../data/index';
+
 const KelasPage = () => {
   return (
-    <div>
-      KelasPage
+    <div className='kelas'>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Semua Kelas</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
