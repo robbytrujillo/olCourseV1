274e353ec82723a0db689ded21222a3b7ec3bec3
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-19 13:39:30 +0700

    setting navbar project olCourseV1 App | testimonial page

--- src/pages/TestimonialPage.jsx
+++ src/pages/TestimonialPage.jsx
@@ -1,7 +1,39 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import {testimonial} from '../data/index';
+
+import FaqComponent from '../components/FaqComponent';
+
 const TestimonialPage = () => {
   return (
-    <div>
-      TestimonialPage
+    <div className='testimonial-page'>
+      <div className="testimonial">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className='fw-bold text-center'>Semua Testimonial</h1>
+              <p className='text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolore.</p>
+            </Col>
+          </Row>
+          <Row className='row-cols-lg-3 row-cols-1'>
+          {testimonial.map((data) => {
+                return(
+                  <Col key={data.id} className='mb-5'>
+                    <p className='desc shadow-sm '>{data.desc}</p>
+                    <div className='people'>
+                      <img src={data.image} alt="" />
+                      <div>
+                        <h5 className='mb-1'>{data.name}</h5>
+                        <p className='m-0 fw-bold'>{data.skill}</p>
+                      </div>
+                    </div>
+                  </Col>
+                ) 
+              })}
+          </Row>
+        </Container>
+      </div>
+
+      <FaqComponent />
     </div>
   )
 }
