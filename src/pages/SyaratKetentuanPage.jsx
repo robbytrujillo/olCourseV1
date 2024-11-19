ee1f7419d613a0526e52cb117e8df558eab6b376
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-19 14:35:50 +0700

    setting navbar project olCourseV1 App | syarat & ketentuan page

--- src/pages/SyaratKetentuanPage.jsx
+++ src/pages/SyaratKetentuanPage.jsx
@@ -1,7 +1,51 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import FaqComponent from '../components/FaqComponent';
+
 const SyaratKetentuanPage = () => {
   return (
-    <div>
-      SyaratKetentuanPage
+    <div className='syarat-ketentuan-page'>
+      <div className='syarat-ketentuan min-vh-100'>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className='fw-bold text-center mb-2'>
+                Syarat & Ketentuan
+              </h1>
+              <p className='text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row className='pt-5'>
+            <Col>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum repudiandae placeat, beatae facere quis adipisci, sed sapiente amet culpa consequuntur earum iste dolor, labore provident est expedita autem assumenda ullam illo quibusdam officiis! Maxime culpa facilis vitae sapiente. Dolores sit qui repudiandae fugiat cumque voluptatem, officia dolore. Quis, ducimus nihil placeat ullam eius excepturi voluptatem provident minus atque distinctio consequatur!</p>
+            </Col>
+          </Row>
+          <Row className='py-3'>
+            <Col>
+              <h4 className='fw-bold'>1. Lorem</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, officia. Eius deserunt mollitia incidunt eum perspiciatis aliquid ullam cumque sequi. Nesciunt reprehenderit nostrum omnis incidunt in, fuga amet rem corrupti eaque velit distinctio officia animi. Dolorem quas veritatis commodi numquam.</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti obcaecati, est sed harum voluptatibus fugiat quidem quam ipsum sequi enim suscipit atque ab architecto pariatur assumenda nobis maxime veniam quod temporibus sit quae iusto aspernatur? Non vitae esse autem possimus tempora facere, obcaecati aspernatur in distinctio! Consectetur eos harum error earum molestias eum molestiae, cum ab repudiandae magnam corrupti et!</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, maiores repellendus voluptates non, perferendis laboriosam nesciunt rerum dolor ducimus vitae nulla. Dicta id quis illum deleniti eaque ea reprehenderit, sed esse aperiam iste cumque odit, molestias laboriosam numquam rem harum voluptatum, veniam sequi iure officia nostrum provident excepturi tempore unde. Tempore eaque labore nostrum est non reprehenderit debitis necessitatibus vero alias. Perspiciatis in doloribus omnis enim, cum accusamus, iusto excepturi aliquid quae ipsam quas perferendis distinctio eius, unde repudiandae recusandae.</p>
+            </Col>
+          </Row>
+          <Row className='py-3'>
+            <Col>
+              <h4 className='fw-bold'>2. Lorem</h4>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, vel necessitatibus. Optio perspiciatis ducimus consectetur, dolore accusamus, voluptates velit alias odio ipsum modi a. Cum, numquam eaque iusto quidem placeat mollitia quis in ex animi, praesentium maxime perspiciatis. Eius error, veritatis consectetur dolore fuga in repellat sunt aperiam ducimus repudiandae corrupti similique hic, iure non reprehenderit saepe pariatur quis corporis.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error impedit labore eligendi sint, molestiae, sapiente ex repellat veniam ad voluptatem alias soluta quam dolores aliquam! At quis ducimus laudantium. Aperiam ullam ratione odit alias ducimus provident commodi totam temporibus voluptate atque nisi, minima praesentium amet dolorum eligendi unde maxime similique veritatis facilis quidem voluptatem. Ullam, doloremque praesentium. Quaerat similique expedita voluptates ipsam a dolor reiciendis vitae eligendi, in deserunt adipisci. Est fugiat nemo quos culpa quis dolorem fugit molestiae aliquid.</p>
+            </Col>
+          </Row>
+          <Row className='py-3'>
+            <Col>
+              <h4 className='fw-bold'>3. Lorem</h4>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis incidunt officia quibusdam voluptate similique deserunt animi necessitatibus corrupti beatae reiciendis aliquid molestiae, esse dolorum ratione velit aliquam quo ipsum est eaque repudiandae! Distinctio provident dignissimos explicabo veritatis minus nobis temporibus.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dicta debitis, sed ipsam quaerat, magnam illo maiores molestiae veritatis, cum vel molestias officia neque? Cum odio at maiores cupiditate modi similique corrupti fugit blanditiis, dicta sequi veritatis! Reprehenderit non voluptate dignissimos magni saepe id. Quas maiores maxime molestias. Maiores eius suscipit soluta a ullam non officia laudantium, minus tempora dignissimos quisquam dicta sunt autem delectus ea! Blanditiis consequuntur exercitationem excepturi ut? Blanditiis obcaecati rerum quis asperiores aspernatur tenetur quaerat totam!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, consectetur ex perspiciatis id consequatur magnam officia quam exercitationem explicabo cupiditate, placeat possimus quod illo eius! Neque corrupti dolore aliquid, accusantium labore expedita necessitatibus modi et repudiandae, quas aperiam velit vero error iure repellendus similique odio! Quam accusamus animi nam sunt optio aspernatur sed, et natus corporis nostrum tempore pariatur unde.</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      
+      <FaqComponent />
     </div>
   )
 }
